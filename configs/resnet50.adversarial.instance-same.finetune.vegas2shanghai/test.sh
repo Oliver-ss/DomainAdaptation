ea986a72c6a1fb5703404676f3723fe3d79cc1db
Oliver_ss <yuansongwx@outlook.com>
2020-01-16 23:02:51 -0500

    update

--- configs/resnet50.adversarial.instance-same.finetune.vegas2shanghai/test.sh
+++ configs/resnet50.adversarial.instance-same.finetune.vegas2shanghai/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -o train_out
-#SBATCH -e train_err
+#SBATCH -o test_out
+#SBATCH -e test_err
 #SBATCH --gres=gpu:1
 make test MODEL=train_log/models/best.pth BN=False SAVE=0
 make test MODEL=train_log/models/best.pth BN=True SAVE=0
